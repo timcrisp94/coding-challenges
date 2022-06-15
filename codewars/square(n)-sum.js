d08b8dfb4aa91f67c3217f8475ce2906f12072c1
Tim Crisp <timothycrisp@gmail.com>
2022-06-15 09:54:41 -0500

    init commit

---
+++ codewars/square(n)-sum.js
@@ -0,0 +1,24 @@
+
+/*
+
+https://www.codewars.com/kata/515e271a311df0350d00000f/train/javascript
+
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+*/
+
+// for each number
+  // square each number and add the sum of those squared numbers together
+// return the sum
+
+function squareSum(numbers) {
+  let result = 0
+  for (let number of numbers) {
+    result += number * number
+  }
+  return result
+}
+
+console.log(squareSum([1, 2, 2]))
