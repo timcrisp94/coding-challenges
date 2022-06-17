0d4803411d217e2edfe5df954d7278f239b16cdd
Tim Crisp <timothycrisp@gmail.com>
2022-06-17 11:25:35 -0500

    solve disemvowel with iterative solution

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 console.log('hi')
+
+function disemvowel(str) {
+  return str.replace(/[aeiou]/gi, '')
+}
+
+console.log(disemvowel("N ffns bt,\nYur wrtng s mng th wrst \'v vr rad"))
---
+++ codewars/disemvowel.js
@@ -0,0 +1,26 @@
+/*
+
+Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.
+
+*/
+
+function disemvowel(str) {  
+  const checker = ['a', 'e', 'i', 'o', 'u']
+  let newString = ''
+
+  for (let i = 0; i <= str.length; i++) {
+    let char = str.charAt(i)
+    if (checker.indexOf(char) == -1) {
+      newString += char
+    }    
+  }  
+  return newString  
+}
