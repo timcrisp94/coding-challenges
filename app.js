331e144ea1de2c22c7b219930c98001a03dd30a4
Tim Crisp <timothycrisp@gmail.com>
2022-06-17 10:45:51 -0500

    solve moveZeros with iterative solution

--- app.js
+++ app.js
@@ -1,49 +1,31 @@
 console.log('hi')
+
 /*
-There's a waiting room with N chairs set in single row. Chairs are consecutively numbered from 1 to N. First is closest to the entrance (which is exit as well).
 
-For some reason people choose a chair in the following way
-
-Find a place as far from other people as possible
-Find a place as close to exit as possible
-All chairs must be occupied before the first person will be served
-
-So it looks like this for 10 chairs and 10 patients
-Chairs	  [X	2	3	4	5]	[6	7	8	9	 X]
-Patients	[1	7	5	8	3]  [9	4	6	10	2]
-
-
-
-
-your task is to find last patient's chair's number
-
-Input: number of chairs N, an integer greater than 2.
-Output: a positive integer, the last patient's chair number.
+Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
 
 */
 
-// n chairs
-// as far as possible
-// as close to the exit as possible
+console.log([ 9, 0, 9, 1, 2, 1, 1, 3, 1, 9, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0 ])
 
-// left, right, center -> 1 , 10, 5
-// center, left, right -> 7, 3, 8 
-// right, center, left -> 2, 4, 6
-// [
-//   1, 10, 5, 
-//   7, 3, 8,
-//   2, 4, 6, 
-//   9
-// ]
+function moveZeros(arr) {
+  const newArr = []
+  const zeros = []
 
-// 1, 3, 2
-// 1, 3, 4, 2
-// 1, 4, 3, 5, 2
-// 1, 4, 3, 5, 6, 2
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      zeros.push(arr[i])
+    } else {
+      newArr.push(arr[i])
+    }
+  }
 
-
-function lastChair(N){
-  return N - 1
+  for (el of zeros) {
+    newArr.push(el)
+  }
+  return newArr
 }
 
-console.log(lastChair(10))
+console.log(moveZeros([ 9, 0, 9, 1, 2, 1, 1, 3, 1, 9, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0 ]))
+
+// returns[false,1,1,2,1,3,"a",0,0]
---
+++ codewars/movezeros.js
@@ -0,0 +1,10 @@
+/*
+
+Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+*/
+
+function moveZeros(arr) {
+  return
+}
+
