c9786ec657165c39ab9c75f813b42d5b37417e49
Tim Crisp <timothycrisp@gmail.com>
2022-06-17 11:36:23 -0500

    solve paperwork

--- codewars/paperwork.js
+++ codewars/paperwork.js
@@ -1,5 +1,3 @@
-console.log('hi')
-
 /*
 your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
 
@@ -9,9 +7,7 @@ Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 re
 function paperwork(n, m) {
   if (n < 0 || m < 0) {
     return 0
-  }
-
-  return n * m
+  } 
 }
 
-console.log(paperwork(5, 5))
+console.log(paperwork(0, 2))
