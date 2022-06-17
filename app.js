13f3717919bf69acc61bf21a65f30e238848f578
Tim Crisp <timothycrisp@gmail.com>
2022-06-17 12:01:42 -0500

    add reduce method solution to count postives sum negatives

--- app.js
+++ app.js
@@ -14,24 +14,9 @@ For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should r
 
 */
 
-function countPositivesSumNegatives(input) {
-  const solution = []
-  let count = 0
-  let sum = 0
-  
-  if (input === null || input.length === 0) {
-    return []
-  }
 
-  for (element of input) {
-    if (element > 0) {
-      count++
-    } else {
-      sum = sum + element
-    }
-  }
 
-  return [count, sum]
-}
+
+
 
 console.log(countPositivesSumNegatives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14]))
--- codewars/countPositivesSumNegatives.js
+++ codewars/countPositivesSumNegatives.js
@@ -11,3 +11,39 @@ For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should r
 
 
 */
+
+function countPositivesSumNegatives(input) {
+  const solution = []
+  let count = 0
+  let sum = 0
+  
+  if (input === null || input.length === 0) {
+    return []
+  }
+
+  for (element of input) {
+    if (element > 0) {
+      count++
+    } else {
+      sum = sum + element
+    }
+  }
+
+  return [count, sum]
+}
+
+console.log(countPositivesSumNegatives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14]))
+
+/*
+
+function countPositivesSumNegatives(input) {
+  if (!Array.isArray(input) || !input.length) return []
+
+  return input.reduce((arr, n) => {
+    if (n > 0) arr[0]++
+    if (n < 0) arr[1] += n
+    return arr
+  }, [0, 0])
+}
+
+*/
