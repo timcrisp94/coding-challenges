2049afb6d5e18b27e461504fe09be067443b8a31
Tim Crisp <timothycrisp@gmail.com>
2022-06-19 11:57:22 -0500

    solve fake binary with iteration

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
 /*
 
+Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+Note: input will never be an empty string
+
+
+
 */
+
+function fakeBin(x) {
+  const array = x.split('')
+  let result = []
+
+  for (let digit of array) {
+    if (parseInt(digit) < 5) {
+      result.push(0)
+    } else if (parseInt(digit) >= 5) {
+      result.push(1)
+    }
+  }
+
+  return result.join('')
+}
+
+console.log(fakeBin('156'))
