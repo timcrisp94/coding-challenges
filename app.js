2684d66a7973cabace19891896804e60b3c4096a
Tim Crisp <timothycrisp@gmail.com>
2022-06-21 06:14:17 -0500

    add tribonacci and isIsogram problems

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
 /*
 
-Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
-
-solution('abc', 'bc') // returns true
-solution('abc', 'd') // returns false
 */
 
 
-function solution(str, ending) {
-  return str.endsWith(ending)
-
-}
-
-
-console.log(solution('abc', 'bc'))
 
 /*
-
 
 
 */
---
+++ codewars/isIsogram.js
@@ -0,0 +1,18 @@
+/*
+An isogram is a word that has no repeating letters, consecutive or non-consecutive. 
+Implement a function that determines whether a string that contains only letters is an isogram. 
+Assume the empty string is an isogram. Ignore letter case.
+
+"Dermatoglyphics" --> true
+"aba" --> false
+"moOse" --> false (ignore letter case)
+*/
+
+
+function isIsogram(str) {
+  
+
+}
+
+
+console.log(isIsogram("aba"))
