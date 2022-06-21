bbe12b6843f60e32f1008efd28c07f15c11c4ac5
Tim Crisp <timothycrisp@gmail.com>
2022-06-21 10:27:10 -0500

    solve sum of odd numbers

--- codewars/tribonacci.js
+++ codewars/tribonacci.js
@@ -40,4 +40,14 @@ function fib(n) {
   return result[n];
 }
 
+
+/ solution
+
+function tribonacci(signature,n){  
+  for (var i = 0; i < n-3; i++) { // iterate n times
+    signature.push(signature[i] + signature[i+1] + signature[i+2]); // add last 3 array items and push to trib
+  }
+  return signature.slice(0, n); //return trib - length of n
+  
 */
+
