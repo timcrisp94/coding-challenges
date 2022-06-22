be5161df73dbdfa38178e469eb6b5c400c59eda5
Tim Crisp <timothycrisp@gmail.com>
2022-06-22 07:25:35 -0500

    add iterative solution with stronger run time and reduce solution to positive sum

--- codewars/positiveSum.js
+++ codewars/positiveSum.js
@@ -8,11 +8,32 @@ Note: if there is nothing to sum, the sum is default to 0.
 */
 
 function positiveSum(arr) {
+  let total = 0
+
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      total += arr[i]
+    }
+  }
+
+  return total
+}
+
+console.log(positiveSum([1, -4, 7, 12]))
+
+
+function positiveSumReduce(arr) {
+  return arr.reduce((a, b) => a + (b > 0 ? b : 0), 0)
+}
+
+/*
+
+function positiveSum(arr) {
   const plusArr = [0]
   let sum = 0
 
   for (let num of arr) {
-     if (num >= 0) {
+     if (num > 0) {
         plusArr.push(num)
      }
   }
@@ -24,4 +45,4 @@ function positiveSum(arr) {
   return sum
 }
 
-console.log(positiveSum([1, -4, 7, 12]))
+*/
