e163550932539bed9681a10500057d29b8db06e1
Tim Crisp <timothycrisp@gmail.com>
2022-06-27 10:32:46 -0500

    solve sumtwosmallestnumbers using sort method

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-function paperwork(n, m) {
-   if (n < 0 || m < 0) {
-     return 0
-   } 
- 
-   return n * m
-}
+/*
 
-function paperTern(n, m) {
-   return n < 0 || m < 0 ? 0 : n * m
-}
+Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
 
-console.log(paperwork(5, 2) + ' ; ' + paperTern(5, 2))
+For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+[10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+*/
+
+function sumTwoSmallestNumbers(numbers) {  
+   numbers = numbers.sort(function(a, b) {
+      return a - b
+   })
+   
+   return numbers[0] + numbers[1] 
+ }
+
+console.log(sumTwoSmallestNumbers([19, 5, 42, 2, 77]))
