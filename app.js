6d334e16a9f3c1f0a0a4e0c2632903e84e452165
Tim Crisp <timothycrisp@gmail.com>
2022-06-27 10:51:05 -0500

    solve binary array to number using parseInt and join method

--- app.js
+++ app.js
@@ -1,29 +1,17 @@
 /*
 
-Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+Given an array of ones and zeroes, convert the equivalent binary value to an integer.
 
-Examples (Input -> Output)
-* [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
-* [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
-* [42, 54, 65, 87, 0]             -> min = 0, max = 87
-* [5]                             -> min = 5, max = 5
-Notes
-You may consider that there will not be any empty arrays/vectors.
+Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+Examples:
 
 */
 
-var min = function(list){
-   list = list.sort(function(a, b) {
-      return a - b
-   }) 
-   return list[0];
-}
+const binaryArrayToNumber = arr => {
+   return parseInt(arr.join(''), 2)
+ };
 
-var max = function(list){
-   list = list.sort(function(a, b) {
-      return b - a
-   })
-   return list[0];
-}
 
-console.log(min([4,6,2,1,9,63,-134,566]), max([4,6,2,1,9,63,-134,566]))
+
+console.log(binaryArrayToNumber([0, 0, 0, 1]))
