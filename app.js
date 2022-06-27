5a80ef73d9cffc2e1cdb221047750cd5e3e266c0
Tim Crisp <timothycrisp@gmail.com>
2022-06-27 10:40:56 -0500

    solve max and min values of a list using sort

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 /*
 
-Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
 
-For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
-
-[10, 343445353, 3453445, 3453545353453] should return 3453455.
+Examples (Input -> Output)
+* [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+* [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
+* [42, 54, 65, 87, 0]             -> min = 0, max = 87
+* [5]                             -> min = 5, max = 5
+Notes
+You may consider that there will not be any empty arrays/vectors.
 
 */
 
-function sumTwoSmallestNumbers(numbers) {  
-   numbers = numbers.sort(function(a, b) {
+var min = function(list){
+   list = list.sort(function(a, b) {
       return a - b
-   })
-   
-   return numbers[0] + numbers[1] 
- }
+   }) 
+   return list[0];
+}
 
-console.log(sumTwoSmallestNumbers([19, 5, 42, 2, 77]))
+var max = function(list){
+   list = list.sort(function(a, b) {
+      return b - a
+   })
+   return list[0];
+}
+
+console.log(min([4,6,2,1,9,63,-134,566]), max([4,6,2,1,9,63,-134,566]))
