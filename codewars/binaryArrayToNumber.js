8ec492632014360e2957a1d8d27f0888fdd999fd
Tim Crisp <timothycrisp@gmail.com>
2022-06-27 10:54:06 -0500

    add reduce method solution to binary array to number

---
+++ codewars/binaryArrayToNumber.js
@@ -0,0 +1,22 @@
+/*
+
+Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+Examples:
+
+*/
+
+const binaryArrayToNumber = arr => {
+  return parseInt(arr.join(''), 2)
+};
+
+
+function binaryArrayToNumberReduce(arr) {
+  return arr.reduce((a, b) => a << 1 | b)
+}
+
+
+console.log(binaryArrayToNumber([0, 0, 0, 1]))
+console.log(binaryArrayToNumberReduce([0, 0, 0, 1]))
