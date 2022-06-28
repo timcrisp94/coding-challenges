7e94d56cad256f37231daea758668585399b64e6
Tim Crisp <timothycrisp@gmail.com>
2022-06-28 16:53:51 -0500

    solve add two number w/ linked lists as arrays

--- app.js
+++ app.js
@@ -1,38 +1,20 @@
 /*
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
 
-Given an array of integers nums and an integer target, return indices of the two numbers that add up to the target.
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-You can return the answer in any order.
-
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
 */
 
-var twoSum = function(nums, target) {
-  const object = {}
-  for (let i = 0; i < nums.length; i++) {
-    let cur = nums[i]
-    let diff = target - cur
-    // return answer if current number was a difference from previous number
-    if (object[cur] !== undefined) {
-      return [object[cur], i]  
-      // else set diff to current index in object
-    } else {
-      object[diff] = i
-    }    
-    console.log(object[cur])
-  }
-  
-}  
+var addTwoNumbers = function(l1, l2) {
+   const sum = parseInt(l1.reverse().join('')) + parseInt(l2.reverse().join(''))
+   let array = sum.toString().split('').reverse()
+   return array.map(x => parseInt(x))
+};
 
-
-// console.log(twoSum([2, 7, 11, 5], 9))
-// console.log(twoSum([2, 5, 5, 11], 10))
-// console.log(twoSum([3, 2, 4], 6))
-console.log(twoSum([3, 2, 3], 6))
+console.log(addTwoNumbers([2,4,3],[5,6,4]))
 /*
 
 
