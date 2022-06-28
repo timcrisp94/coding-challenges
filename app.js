04b02d1dfcbb140c0e2205cc756b418e75b02180
Tim Crisp <timothycrisp@gmail.com>
2022-06-28 09:47:25 -0500

    solve vowel count with for of loop

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
-/*
+function getCount(str) {
+  let count = 0
+  const vowels = 'aeiou'
 
-The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+  for (let char of str.split('')) {
+    if (vowels.includes(char)) {
+      count += 1
+    }
+  }
 
-"din"      =>  "((("
-"recede"   =>  "()()()"
-"Success"  =>  ")())())"
-"(( @"     =>  "))((" 
-
-*/
-
-
-function duplicateEncode(word){
-  return word
-  .toLowerCase()
-  .split('')
-  .map(function(a, i, w) {
-    return w.indexOf(a) === w.lastIndexOf(a) ? '(' : ')'
-  })
-  .join('')
+  return count
 }
 
-console.log(duplicateEncode('recede'))
+console.log(getCount('fart'))
