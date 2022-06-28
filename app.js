cea0ca8bc312f4d04778c29ebba04444c45c66df
Tim Crisp <timothycrisp@gmail.com>
2022-06-28 13:46:37 -0500

    solve int to roman

--- app.js
+++ app.js
@@ -9,80 +9,51 @@ L             50
 C             100
 D             500
 M             1000
-
-For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
 
 Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
 
 I can be placed before V (5) and X (10) to make 4 and 9. 
 X can be placed before L (50) and C (100) to make 40 and 90. 
 C can be placed before D (500) and M (1000) to make 400 and 900.
-Given a roman numeral, convert it to an integer.
-
- 
-
-Example 1:
-
-Input: s = "III"
-Output: 3
-Explanation: III = 3.
-Example 2:
-
-Input: s = "LVIII"
-Output: 58
-Explanation: L = 50, V= 5, III = 3.
-Example 3:
-
-Input: s = "MCMXCIV"
-Output: 1994
-Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
-
-
-
+Given an integer, convert it to a roman numeral.
 */
 
 
-var romanToInt = function(s) {
-  // convert roman numerals to numeric values
-    const symbols = {
-      'I': 1,
-      'V': 5,
-      'X': 10,
-      'L': 50,
-      'C': 100,
-      'D': 500,
-      'M': 1000,
-    }
+var intToRoman = function(num) {
+  let result = ''
+  const symbols = {
+    M:  1000,
+    CM: 900,
+    D:  500,
+    CD: 400,
+    C:  100,
+    XC: 90,
+    L:  50,
+    XL: 40,
+    X:  10,
+    IX: 9,
+    V:  5,
+    IV: 4,
+    I:  1,
+  };
 
-    let result = 0
+  if (num === 0) {
+    return ''
+  }
 
-    for (let i = 0; i < s.length; i++) {
-      const cur = symbols[s[i]];
-      const next = symbols[s[i + 1]]
-      // when to add, subtract
-      if (cur < next) {
-        result += next - cur
-        i++
-      } else {
-        result += cur
-      }
-    }
-    // return total
-    return result
+  for (let prop in symbols) {
+    result += prop.repeat(Math.floor(num / symbols[prop]))
+    num %= symbols[prop]
+  }
+
+  return result
 };
 
-console.log(romanToInt("V"))
-
-
-
+console.log(intToRoman(316))
 
 
 /*
-I             1
-V             5
-X             10
-L             50
-C             100
-D             500
-M             1000
+
+
 */
