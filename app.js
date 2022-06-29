d1003a46402c774722a6a87ff686961e9190acf6
Tim Crisp <timothycrisp@gmail.com>
2022-06-29 10:08:05 -0500

    solve reverse-string

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 /*
-
+Given a string, return a new string with the reversed order of characters
 */
 
+function reverse(str) {
+   return str.split('').reverse().join('')
+}
 
-
-
+console.log(reverse('apple'))
 /*
 
 */
