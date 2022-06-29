f9f820792d27326cc636b89808c999313cb1bd89
Tim Crisp <timothycrisp@gmail.com>
2022-06-29 11:57:13 -0500

    solve chunk

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
 /*
-write a program that console logs the numbers from 1 to n
--for multiples of three print fizz
--for multiples of five print buzz
--for multiples of three and five print fizzbuzz
+given an array and chunk size as arguments, divide the array into sub-arrays where each sub-array is of length size
+
+chunk([1, 2, 3, 4, 5], 2)) -> [[1, 2], [3, 4], [5]]
 
 */
 
-function fizzbuzz(n) {
-   for (let i = 1; i <= n; i++) {
-      if (i % 3 === 0 && i % 5 === 0) {
-         console.log('fizzbuzz')
-      } else if (i % 5 === 0) {
-         console.log('buzz')
-      } else if (i % 3 === 0) {
-         console.log('fizz')
-      } else {
-         console.log(i)
-      }
+function chunk(array, size) {
+   const chunked = []
+   let index = 0
+
+   while (index < array.length) {
+      chunked.push(array.slice(index, index + size))
+      index += size
    }
+
+   return chunked
 }
 
-console.log(fizzbuzz(15))
-
-
-
+console.log(chunk([1, 2, 3, 4, 5], 2))
 
 /*
+function chunk(array, size) {
+   const chunked = []
+
+   for (let element of array) {
+      const last = chunked[chunked.length - 1]      
+      if (!last || last.length === size) {
+         chunked.push([element]) 
+      } else {
+         last.push(element)
+      }
+   }
+   return chunked
+}
 
 
 */
---
+++ coding-interview/chunk.js
@@ -0,0 +1,38 @@
+/*
+given an array and chunk size as arguments, divide the array into sub-arrays where each sub-array is of length size
+
+chunk([1, 2, 3, 4, 5], 2)) -> [[1, 2], [3, 4], [5]]
+
+*/
+
+function chunk(array, size) {
+  const chunked = []
+  let index = 0
+
+  while (index < array.length) {
+     chunked.push(array.slice(index, index + size))
+     index += size
+  }
+
+  return chunked
+}
+
+console.log(chunk([1, 2, 3, 4, 5], 2))
+
+/*
+function chunk(array, size) {
+  const chunked = []
+
+  for (let element of array) {
+     const last = chunked[chunked.length - 1]      
+     if (!last || last.length === size) {
+        chunked.push([element]) 
+     } else {
+        last.push(element)
+     }
+  }
+  return chunked
+}
+
+
+*/
