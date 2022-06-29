0f9472350ee8436e461cd0222d30313047434f24
Tim Crisp <timothycrisp@gmail.com>
2022-06-29 10:49:24 -0500

    add palindrome solution using every method

--- app.js
+++ app.js
@@ -2,12 +2,33 @@
 Given a string, return true if the string is a palindrome, false if not
 */
 
-function palidrome(str) {
-   str === str.split('').reverse().join('') ? true : false
+function palindrome(str) {
+   return str.split('').every((char, i) => {
+      return char === str[str.length - i - 1]
+   })
+   
 }
 
 console.log(palindrome('apple'))
 console.log(palindrome('racecar'))
+
 /*
+
+/ two solutions using manually reversed str
+
+function palindrome(str) {
+   if (str === str.split('').reverse().join('')) {
+      return true
+   }
+   return false
+}
+
+function palindrome(str) {
+   const reverse = str.split('').reverse().join('')
+   if (str === reverse) {
+      return true
+   }
+   return false
+}
 
 */
