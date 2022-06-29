c98028da4a26825c0f6399743ac885e9a469224a
Tim Crisp <timothycrisp@gmail.com>
2022-06-29 11:33:17 -0500

    solve maxChar, add charMap.js reference

--- coding-interview/maxChar.js
+++ coding-interview/maxChar.js
@@ -20,22 +20,22 @@ for (let char of string) {
 */
 
 function maxChar(str) {
-   const charMap = {}
-   let max = 0
-   let maxChar = ''
+  const charMap = {}
+  let max = 0
+  let maxChar = ''
 
-   for (let char of str) {
-      charMap[char] = charMap[char] + 1 || 1
-   }
+  for (let char of str) {
+     charMap[char] = charMap[char] + 1 || 1
+  }
 
-   for (let char in charMap) {
-      if (charMap[char] > max) {
-         max = charMap[char]
-         maxChar = char
-      }
-   }
+  for (let char in charMap) {
+     if (charMap[char] > max) {
+        max = charMap[char]
+        maxChar = char
+     }
+  }
 
-   return maxChar
+  return maxChar
 }
 
 console.log(maxChar('apple'))
---
+++ coding-interview/reverseInt.js
@@ -0,0 +1,35 @@
+/*
+given an integer, return an integer that is the reverse ordering of the number
+*/
+
+function reverseInt(n) {
+  const string = n.toString().split('').reverse().join('')   
+  return parseInt(string) * Math.sign(n)   
+}
+
+console.log(reverseInt(15))
+console.log(reverseInt(981))
+console.log(reverseInt(-12))
+console.log(reverseInt(0))
+
+/*
+
+/ first solution
+
+function reverseInt(n) {
+  const string = n.toString().split('').reverse().join('')
+  
+  if (n === 0) {
+     return 0
+  }
+
+  if (Math.sign(n) === -1) {
+     return parseInt(string) * -1
+  } else {
+     return parseInt(string)
+  }  
+  
+}
+
+
+*/
