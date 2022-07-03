e4b903a8959570382bd254c321a8a5dc0b843402
Tim Crisp <timothycrisp@gmail.com>
2022-07-03 07:38:32 -0500

    solve array diff with for of and filter method

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 /*
+Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
 
+It should remove all values from list a, which are present in list b keeping their order.
+
+arrayDiff([1,2],[1]) == [2]
 */
+function arrayDiff(a, b) {
+  return a.filter(function(x) {
+    return b.indexOf(x) < 0
+  })
+}
 
-
+console.log(arrayDiff([1,2], [1]))
 /*
+function arrayDiff(a, b) {
+  let result = []
 
+  for (let num of a) {
+    if (!b.includes(num)) {
+      result.push(num)
+    }
+  }
+  return result
+}
 
 */
