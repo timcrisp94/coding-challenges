4a259889ebea5f89db97cb337eeb36e447194e25
Tim Crisp <timothycrisp@gmail.com>
2022-07-03 07:51:28 -0500

    solve likes(names) with return object and conditionals

--- app.js
+++ app.js
@@ -1,27 +1,40 @@
 /*
-Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
 
-It should remove all values from list a, which are present in list b keeping their order.
-
-arrayDiff([1,2],[1]) == [2]
+Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
 */
-function arrayDiff(a, b) {
-  return a.filter(function(x) {
-    return b.indexOf(x) < 0
-  })
+function likes(names) {
+  return {
+    0: 'no one likes this',
+    1: `${names[0]} likes this`, 
+    2: `${names[0]} and ${names[1]} like this`, 
+    3: `${names[0]}, ${names[1]} and ${names[2]} like this`, 
+    4: `${names[0]}, ${names[1]} and ${names.length - 2} others like this`, 
+  }[Math.min(4, names.length)]
+  
 }
 
-console.log(arrayDiff([1,2], [1]))
-/*
-function arrayDiff(a, b) {
-  let result = []
+console.log(likes(["Alex", "Jacob", "Mark", "Max"]))
 
-  for (let num of a) {
-    if (!b.includes(num)) {
-      result.push(num)
-    }
+/*
+
+function likes(names) {
+  if (names.length === 0) {
+    return "no one likes this"
+  } 
+  if (names.length === 1) {
+    return `${names[0]} likes this`
+  } 
+  if (names.length === 2) {
+    return `${names[0]} and ${names[1]} like this`
   }
-  return result
+  if (names.length === 3) {
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`
+  }
+
+  return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+  
+  
 }
 
 */
