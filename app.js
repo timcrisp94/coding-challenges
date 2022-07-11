adc8876d2a76ffba19ce3c494c238eef8917a25d
Tim Crisp <timothycrisp@gmail.com>
2022-07-11 15:46:57 -0500

    solve product except self at o(n)

--- app.js
+++ app.js
@@ -1,42 +1,45 @@
 /*
-Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+ 
 
 Example 1:
 
-Input: nums = [1,2,3,1]
-Output: true
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
 Example 2:
 
-Input: nums = [1,2,3,4]
-Output: false
-Example 3:
-
-Input: nums = [1,1,1,3,3,4,3,2,4,2]
-Output: true
-
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
 */
 
-// brute force
-// var containsDuplicate = function (nums) {
-//   let results = []
+var productExceptSelf = function(nums) {
+  let len = nums.length
+  let result = Array(len) // [ , , , ]
+  let left = Array(len + 1)
+  let right = Array(len + 1)
 
-//   for (let num of nums) {
-//     if (!results[num]) {
-//       results.push(num)
-//     } else {
-//       return true
-//     }
-//     return false
-//   }  
-// }
+  left[0] = 1
+  right[0] = 1
 
-// var containsDuplicate = function(nums) {
-//   let numSet = new Set(nums)
-//   return numSet.size !== nums.length
-// }
+  for (let i = 0; i < len; i++) {
+    left[i + 1] = left[i] * nums[i]   
+  }
 
-var containsDuplicate = function(nums) {
-  return (new Set(nums).size) !== nums.length
-}
+  for (let j = 0; j < len; j++) {
+    right[j + 1] = right[j] * nums[len - 1 - j]    
+  }
 
-console.log(containsDuplicate([1,2,1]))
+  for (let k = 0; k < len; k++) {
+    result[k] = left[k] * right[len - k - 1]    
+  }
+
+  return result
+
+};
+
+console.log(productExceptSelf([1, 2, 3, 4]))
