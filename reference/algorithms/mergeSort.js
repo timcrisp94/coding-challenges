cec9fbec17ef2396279a7bd86b5f52259514b41c
Tim Crisp <timothycrisp@gmail.com>
2022-07-14 10:42:28 -0500

    create reference folder, add algorithms folder and mergeSort.js

---
+++ reference/algorithms/mergeSort.js
@@ -0,0 +1,27 @@
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr
+
+  const middle = Math.floor(arr.length / 2)
+  const left = arr.slice(0, middle)
+  const right = arr.slice(middle)
+
+  return merge(mergeSort(left), mergeSort(right))
+}
+
+function merge(left, right) {
+  let result = [], leftIndex = 0, rightIndex = 0
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex])
+      leftIndex++
+    } else {
+      result.push(right[rightIndex])
+      rightIndex++
+    }
+  }
+  return result
+  .concat(left.slice(leftIndex))
+  .concat(right.slice(rightIndex))
+}
+
