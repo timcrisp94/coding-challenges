91043d0bdbeae8d52ff55db02d358f6d8284227a
Tim Crisp <timothycrisp@gmail.com>
2022-07-19 11:49:03 -0500

    solve length of longest substring with map object and left/right pointers

--- app.js
+++ app.js
@@ -1,68 +1,33 @@
 /*
-You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
+Given a string s, find the length of the longest substring without repeating characters.
 
-Return the length of the longest substring containing the same letter you can get after performing the above operations.
-
-Input: s = "ABAB", k = 2
-Output: 4
-Explanation: Replace the two 'A's with two 'B's or vice versa.
-Example 2:
-
-Input: s = "AABABBA", k = 1
-Output: 4
-Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
-The substring "BBBB" has the longest repeating letters, which is 4.
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
 */
 
-// binary search
-// set initial values for left and right to first and last numbers of array
-// if input array.length is 1, return number
-// if left val is less than right, the input array is not rotated, return left val
-// binary search for right val less than left
-
-var characterReplacement = function(s, k) {
-  const chars = {}
-  let left = 0
-  let right = 0
-  let maxCharCount = 0
-
-  while (right < s.length) {
-    const char = s[right]
-    chars[char] = chars[char] + 1 || 1
-
-    if (chars[char] > maxCharCount) {
-      maxCharCount = chars[char]
+const lengthOfLongestSubstring = function(s) {
+  // initial map, left pointer, and counter
+  const map = new Map()
+  let left = 0, longestSub = 0
+  // base case
+  if (s.length === 1) return 1
+  // iterate over string, set right pointer variable
+  for (let right = 0; right < s.length; right++) {
+    // initialize currentChar variable, and currentCharPosition (in map)
+    let currentChar = s[right]
+    let currentCharPosition = map.get(currentChar)
+    // if the currentChar exists in the map and was found within the current window
+    while (currentCharPosition >= left) {
+      // move current window
+      left = currentCharPosition + 1
     }
-
-    if (right - left + 1 - maxCharCount > k) {
-      chars[s[left]]--
-      left++
-    }
-    right++
+    // add character to map in its position
+    map.set(currentChar, right)
+    // update longestSub
+    longestSub = Math.max(longestSub, right - left + 1)
   }
-  return right - left
-};
+  return longestSub
+}
 
-console.log(characterReplacement('ABAB', 2))
-
-var characterReplacement2 = function(s, k) {
-  // Keep count of all the characters in the string
-  const chars = {}
-  // left pointer, character with the current max frequency, return value
-  let left = 0, maxf = 0, output = 0
-  for(let right = 0; right < s.length; right++) {
-      const char = s[right]
-      // Increment count of the current character
-      chars[char] = 1 + (chars[char] || 0)
-      // Update the character frequency
-      maxf = Math.max(maxf, chars[char])
-      // Shrink the window of characters we are looking at until we can have a window of all the same characters + k charcters to change
-      while((right-left+1) - maxf > k) {
-              chars[s[left]] -= 1
-              left++
-        }
-      // Update the output if the current window is greater than our previous max window
-      output = Math.max(output, right - left +1)
-  }
-  return output
-};
+console.log(lengthOfLongestSubstring('abcabcbb'))
