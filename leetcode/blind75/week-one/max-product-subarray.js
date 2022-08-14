0243a644df079e4d011ed7ba0520e7501de7cb53
Tim Crisp <timothycrisp@gmail.com>
2022-08-14 14:08:13 -0500

    add pseudocode to group anagram

--- leetcode/blind75/week-one/max-product-subarray.js
+++ leetcode/blind75/week-one/max-product-subarray.js
@@ -13,6 +13,21 @@ Explanation: [2,3] has the largest product 6.
 
 */
 
+/*
+pseudo : dyanmic programming
+  *-track min and max-*
+let result = nums [1]
+set currentMax and currentMin = 1
+for let num of nums
+  e.c./reset if num === 0, currentMax and Min = 1
+  if result < num, result = num
+  temp = currentMax * num
+  currentMax(temp, num * currentMin, num)
+  currentMin(temp, num * currentMin, num)
+  result = max(result, currMax)
+return result  
+*/
+
 const maxProduct = function(nums) {
   let result = nums[0]
   let currentMax = 1
@@ -32,5 +47,10 @@ const maxProduct = function(nums) {
   return result
 }
 
+
+
+
+
 console.log(maxProduct([2, 3, -2, 4]))
 console.log(maxProduct([-2, 0, -1]))
+
