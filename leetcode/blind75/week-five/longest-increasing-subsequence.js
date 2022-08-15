f27f1c7a83213341df6d4c24eeee0ee4c2ad5ffc
Tim Crisp <timothycrisp@gmail.com>
2022-08-15 10:09:09 -0500

    add binary search solution to length of longest subsequence

--- leetcode/blind75/week-five/longest-increasing-subsequence.js
+++ leetcode/blind75/week-five/longest-increasing-subsequence.js
@@ -5,7 +5,7 @@ Given an integer array nums, return the length of the longest strictly increasin
 A subsequence is a sequence that can be derived from an array by deleting some or no elements without changing the order of the remaining elements. For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
 
 */
-
+// O(n^2) solution 
 const lengthOfLISP = function(nums) {
   let n = nums.length
   // build array length n, fill 
@@ -42,6 +42,33 @@ const lengthOfLIS = function(nums) {
   return Math.max(...arr)
 }
 
+// binary solution
+// 
+const lengthOfLISBinary = function(nums) {
+  let n = nums.length
+  if (n === 0) return 0
 
+  const bsPosition = (dp, target, right) => {
+    let left = 0
+    while (left <= right) {
+      let mid = Math.floor((left + right)/2)
+      if (target === dp[mid]) return mid
+      else if (target < dp[mid]) right = mid - 1
+      else left = mid + 1
+    }
+    return left
+  }
 
-console.log(lengthOfLIS([10,9,2,5,3,7,101,18]))
+  let dp = new Array(n).fill(Number.MAX_SAFE_INTEGER)
+  for (let i = 0; i < n; i++) {
+    let pos = bsPosition(dp, nums[i], i)
+    dp[pos] = nums[i]
+  }
+
+  for (let i = dp.length - 1; i >= 0; i--) {
+    if (dp[i] !== Number.MAX_SAFE_INTEGER) return i + 1
+  }
+  return 0
+}
+
+console.log(lengthOfLISBinary([10,9,2,5,3,7,101,18]))
