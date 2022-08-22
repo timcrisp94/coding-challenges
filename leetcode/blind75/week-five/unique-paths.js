211a40a7adc52652ec622ebc1b72dd86ad5695f5
Tim Crisp <timothycrisp@gmail.com>
2022-08-22 09:15:11 -0500

    solve unique paths with dynamic programming solution, add memoization and dp bottom-up

--- leetcode/blind75/week-five/unique-paths.js
+++ leetcode/blind75/week-five/unique-paths.js
@@ -12,8 +12,75 @@ Explanation: From the top-left corner, there are a total of 3 ways to reach the 
 3. Down -> Right -> Down
 */
 
-const uniquePaths = function(m, n) {
+// const uniquePaths = function(m, n) {
+//   let row = new Array(n).fill(1)
+  
+//   for (let i = 0; i < m - 1; i++) {
+//     let newRow = new Array(n).fill(1)
+//     for (let j = n - 2; j > -1; j--) {
+//       newRow[j] = newRow[j + 1] + row[j]      
+//     }
+//     row = newRow
+//   }
+//   return row[0]
+// }
 
+// refactored from above
+
+const uniquePaths = (m, n) => {
+  const dp = new Array(n + 1).fill(1)
+
+  for (let row = m - 1; row > 0; row--) {
+    for (let col = n - 1; col > 0; col--) {
+      dp[col] = dp[col] + dp[col + 1]
+      
+    }    
+  }
+  return dp[1]
 }
 
 console.log(uniquePaths(3, 7))
+
+// DP bottom up tabular
+
+const uniquePathsBottomUp = (m, n) => {
+  const dp = new Array(m + 1).fill(0)
+  for (let i = 0; i < dp.length; i++) {
+    dp[i] = new Array(n + 1).fill(1)
+  }
+
+  dp[m][n] = 1
+  for (let row = m - 1; row > 0; row--) {
+    for (let col = n -1; col > 0; col--) {
+      dp[row][col] = dp[row+1][col] + dp[row][col + 1]
+    }
+  }
+  return dp[1][1]
+}
+
+// console.log(uniquePathsBottomUp(3, 7))
+
+// using memoization
+const uniquePathsMemo = (m, n) => {
+  const memo = new Array(m + 1).fill(0)
+  for (let i = 0; i < memo.length; i++) {
+    memo[i] = new Array(n + 1).fill(-1)
+  }
+  return memoHelper(m, n, 1, 1, memo)
+}
+
+const memoHelper = (m, n, row, col, memo) => {
+  if (row === m && col === n) return 1
+  if (row > m || col > n) return 0
+
+  if (memo[row][col] === -1) {
+    const pathsRight = memoHelper(m, n, row, col + 1, memo)
+    const pathsDown = memoHelper(m, n, row + 1, col, memo)
+
+    memo[row][col] = pathsRight + pathsDown
+  }
+  return memo[row][col]
+}
+
+
+// console.log(uniquePathsMemo(3, 7))
