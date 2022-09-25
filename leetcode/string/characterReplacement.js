5eb42a5976cc3b2f3c921a637588995c18fa81c0
Tim Crisp <timothycrisp@gmail.com>
2022-09-24 22:38:52 -0500

    temp

--- leetcode/string/characterReplacement.js
+++ leetcode/string/characterReplacement.js
@@ -1,7 +1,4 @@
 /*
-You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
-
-Return the length of the longest substring containing the same letter you can get after performing the above operations.
 
 Input: s = "ABAB", k = 2
 Output: 4
