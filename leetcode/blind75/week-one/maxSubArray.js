4df9f013bc7a98a76bf97e897f70e0573e6f73fd
Tim Crisp <timothycrisp@gmail.com>
2022-09-29 09:09:03 -0500

    add pseudo to max sub array

--- leetcode/blind75/week-one/maxSubArray.js
+++ leetcode/blind75/week-one/maxSubArray.js
@@ -1,3 +1,36 @@
+/*
+
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+A subarray is a contiguous part of an array. 
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+Example 2:
+
+Input: nums = [1]
+Output: 1
+Example 3:
+
+Input: nums = [5,4,-1,7,8]
+Output: 23
+
+- pseudo - 
+* greedy algorithm *
+function (nums)
+N = nums.length
+sum = 0
+maxSum = -Infinity
+FOR let i = 0 UPTO N
+  sum = max(sum + nums[i], nums[i])
+  max = (max, sum)
+return maxSum
+
+*/
+
 // greedy
 
 // function maxSubArray(nums) {
@@ -11,7 +44,7 @@
 //   return maxSum
 // }
 
-// dynamic greedy
+dynamic greedy
 const maxSubArray = (nums) => {
   const n = nums.length
   for (let i = 1; i < n; i++) {
@@ -19,5 +52,6 @@ const maxSubArray = (nums) => {
   }
   return Math.max(...nums)
 }
+
 
 console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
