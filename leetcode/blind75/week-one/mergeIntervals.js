f64ddda8aedd933103b7fafcd1f42b1149aeffdf
Tim Crisp <timothycrisp@gmail.com>
2022-09-30 09:56:51 -0500

    add pseudo code to merge intervals, optimize solution

--- leetcode/blind75/week-one/mergeIntervals.js
+++ leetcode/blind75/week-one/mergeIntervals.js
@@ -1,4 +1,6 @@
 /*
+56. MERGE INTERVALS
+
 Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
 
 Example
@@ -7,20 +9,24 @@ output : [1,6], [8,10], [15,18]
 
 */
 
-// O (n log n)
+// - pseudo -
+// N = intervals.length
 // sort intervals by first element
 // let result = [intervals[0]]
-// iterate through intervals
-  // if intervals[i][0] is less than or equal to last end (result.at(-1)[1])
-    // last end = max(lastend, intervals[i][1])
-  // else push interval to result
-// return result
+// FOR let i = 1 UPTO N
+  // let last = result.at(-1)[1] (same as intervals[0[1]])
+  // let start = intervals[i][0] (first i same as intervals[1[0]])
+  // IF (start <= last)
+    // result.at(-1)[1] = max(last, intervals[i][1])
+  // ELSE  result.push(intervals[i])
+  
 
 var merge = function(intervals) {
+  const n = intervals.length
   intervals.sort((a, b) => a[0] - b[0])
   let result = [intervals[0]]
   
-  for (let i = 1; i < intervals.length; i++) {
+  for (let i = 1; i < n; i++) {
     let lastEnd = result.at(-1)[1]
     let start = intervals[i][0]
 
@@ -35,3 +41,5 @@ var merge = function(intervals) {
 
 console.log(merge([[1,3], [2,6], [8,10], [15,18]]))
 console.log(merge([1,4], [0,4]))
+
+// time complexity : O (n log n) 
