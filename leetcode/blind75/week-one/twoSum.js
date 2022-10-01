5bf27cb4047bb9b4d7cd317626f2ef5ac6fccbab
Tim Crisp <timothycrisp@gmail.com>
2022-10-01 09:25:33 -0500

    add pseudo and desc. to twoSum

--- leetcode/blind75/week-one/twoSum.js
+++ leetcode/blind75/week-one/twoSum.js
@@ -1,8 +1,32 @@
-// input: nums, target (int)
-// return: indices of the two nums that add to target 
-// only one solution
+/*
+1. TWO SUM
 
-// hash map, sliding window
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+- pseudo - 
+*hash map and sliding window*
+const n = nums.length
+hash = {}
+result = []
+FOR let i UPTO n
+  cur = nums[i]
+  dif = target - cur
+  IF dif in hash
+    return [hash[diff], i]
+  map[cur] = i
+(return null)
+*/
+
 
 const twoSum = function(nums, target) {
   const n = nums.length
@@ -22,20 +46,3 @@ const twoSum = function(nums, target) {
 
 
 console.log(twoSum([3,2,4], 6))
-
-/*
-const twoSum = (nums, target) => {
-  let map = new Map()
-
-  for (let i = 0; i < nums.length; i++) {
-    let curr = nums[i]
-    let diff = target - curr
-
-    if (map.has(diff)) {
-      return [map.get(diff), i]
-    }
-    map.set(curr, i)
-  }
-}
-
-*/
