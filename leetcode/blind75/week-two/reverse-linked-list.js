960248f328075a0e1607b00e53c8b8f4ee67cf46
Tim Crisp <timothycrisp@gmail.com>
2022-10-04 08:11:21 -0500

    add pseudo and description to reverse linked list

--- leetcode/blind75/week-two/reverse-linked-list.js
+++ leetcode/blind75/week-two/reverse-linked-list.js
@@ -1,8 +1,28 @@
-// Given the head of a singly linked list, reverse the list, and return the reversed list.
+/*
+206/12. REVERSE LINKED LIST
 
-// Input: head = [1,2,3,4,5]
-// Output: [5,4,3,2,1]
+Given the head of a singly linked list, 
+reverse the list, and return the reversed list.
 
+Example : 
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+in:  (1)->(2)->(3)->(4)->(5)
+out: (5)->(4)->(3)->(2)->(1)
+
+
+
+- pseudo -
+let cur = head
+let prev = null
+WHILE cur
+  let hold = cur.next
+  cur.next = prev
+  prev = cur
+  cur = hold
+return prev
+*/
 
 const reverseList = function(head) {
   let curr = head
