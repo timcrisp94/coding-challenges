aa096b6f2f30008507f25cef8b006a10aedd8efe
Tim Crisp <timothycrisp@gmail.com>
2022-10-18 09:23:56 -0500

    solve pac-atl-water with dfs

--- leetcode/blind75/week-two/pacific-atlantic-waterflow/dfs.js
+++ leetcode/blind75/week-two/pacific-atlantic-waterflow/dfs.js
@@ -19,7 +19,7 @@ Return a 2D list of grid coordinates result where
   *dfs*
 function pacificAtlantic(matrix)
   const m, n = matrix.length, matrix[0].length
-  IF n < 0 return []
+  IF m < 0 return []
   const pacVisited = new Set()
   const atlVisited = new Set()
   const directions = [[1,0], [-1,0], [0,1], [0,-1]]
@@ -45,3 +45,52 @@ function pacificAtlantic(matrix)
     dfs(m-1, col, pacVisited)
   return [...intersection(pacVisited, atlVisited)].map(pair => pair.split('-').map(Number))
 */
+
+const pacificAtlantic = function(matrix) {
+  const [m,n] = [matrix.length, matrix[0].length]
+  if (m === 0) return []
+
+  const pacVisited = new Set()
+  const atlVisited = new Set()
+  const directions = [[1,0],[-1,0],[0,1],[0,-1]]
+
+  const intersection = (setA, setB) => {
+    let result = new Set()
+    for (let el of setB) {
+      if (setA.has(el)) {
+        result.add(el)
+      }
+    }
+    return result
+  }
+  const dfs = (i, j, visited) => {
+    const pair = `${i}-${j}`
+    if (visited.has(pair)) return
+    visited.add(pair)
+    
+    for (const direction of directions) {
+      const [nextI, nextJ] = [i + direction[0], j + direction[1]]
+      if (
+        0 <= nextI &&
+        nextI < m &&
+        0 <= nextJ &&
+        nextJ < n &&
+        matrix[nextI][nextJ] >= matrix[i][j]
+      ) {
+        dfs(nextI, nextJ, visited)
+      }
+    }
+  }
+  for (let row = 0; row < m; row++) {
+    dfs(row, 0, pacVisited)
+    dfs(row, n-1, atlVisited)
+  }
+  for (let col = 0; col < n; col++) {
+    dfs(0, col, pacVisited)
+    dfs(m-1, col, atlVisited)
+  }
+  return [...intersection(atlVisited, pacVisited)].map(pair => pair.split('-').map(Number))
+}
+
+console.log(pacificAtlantic([[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]))
+// Output: [[0,4],[1,3],[1,4],[2,2],[3,0],[3,1],[4,0]]
