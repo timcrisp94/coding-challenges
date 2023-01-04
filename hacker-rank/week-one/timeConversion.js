23122af2dc53cb109033ff17a70fddbdd0a25573
Tim Crisp <timothycrisp@gmail.com>
2023-01-04 10:18:24 -0600

    solve time conversion using an object

--- hacker-rank/week-one/timeConversion.js
+++ hacker-rank/week-one/timeConversion.js
@@ -1,10 +1,36 @@
 /*
 Given a time in -hour AM/PM format, convert it to military (24-hour) time.
 
+SPLIT time into Array
+IF mod === am
+  IF time.substring(0,2) === 12
+    replace 12 w/ 00
+  ELSE return time
+ELSE
+  IF time.substring(0,2) === 12
+    return time
+  ELSE replace w/ 12 + value
 */
 
 function timeConversion(s) {
+    let timeArray = s.split(":")
+    const time = {
+        hours: timeArray[0],
+        minutes: timeArray[1],
+        seconds: timeArray[2].slice(0,2),
+        modifier: timeArray[2].slice(2,4)
+    }
+    
+    if (time.modifier === "PM" && parseInt(time.hours) !== 12) {
+        time.hours = String(parseInt(time.hours) + 12)
+    } else if (time.modifier === "AM" && parseInt(time.hours) === 12) {
+        time.hours = "00"
+    }
+     return `${time.hours}:${time.minutes}:${time.seconds}`
     
 }
 
-console.log(timeConversion("12:01:00PM"))
+console.log(timeConversion("12:01:00AM"))
+console.log(timeConversion("04:01:00PM"))
+console.log(timeConversion("04:01:00AM"))
+console.log(timeConversion("07:05:45PM"))
