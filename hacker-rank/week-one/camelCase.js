389eae45de579a14988a127b336097501f582c34
Tim Crisp <timothycrisp@gmail.com>
2023-01-05 11:40:44 -0600

    add alt solution, refactor camel case solution

--- hacker-rank/week-one/camelCase.js
+++ hacker-rank/week-one/camelCase.js
@@ -1,21 +1,28 @@
 function processData(input) {
     const n = input.length
-    const inputArray = input.split(";")
-    let splitOrCombine = inputArray[0]
-    let type = inputArray[1]
-    let string = inputArray[2]
-    
-    if (splitOrCombine === "S") {
-        let splitOut = string.replace(/([a-zA-Z])(?=[A-Z])/g, "$1 ").toLowerCase()
-        if (type === "M") {
-            return splitOut.replace(/[{()}]/g, '')
-        } else {
-            return splitOut
-        }
+    let splitSemi = input.split("\r\n")
 
-    } else {
-        return string.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase())
-    }
+    splitSemi.forEach((res) => {
+        const inputArray = res.split(";")
+        let splitOrCombine = inputArray[0]
+        let type = inputArray[1]
+        let string = inputArray[2]
+        
+        if (splitOrCombine === "S") {
+            let splitOut = string.replace(/([a-zA-Z])(?=[A-Z])/g, "$1 ").toLowerCase()
+            if (type === "M") {
+                console.log(splitOut.replace(/[{()}]/g, ''))
+            } else {
+                console.log(splitOut)
+            }
+    
+        } else {
+            let combinedOut = string.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase())
+
+            console.log(type === "M" ? combinedOut+"()" : combinedOut)
+        }
+    })
+    
 }
 
 
