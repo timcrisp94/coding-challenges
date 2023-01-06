1cad42f04a2e8357b8e70854c1230906d8a11dca
Tim Crisp <timothycrisp@gmail.com>
2023-01-06 13:56:04 -0600

    solve counting valleys using conditionals and boolean variable

--- hacker-rank/week-two/diagonalDifference.js
+++ hacker-rank/week-two/diagonalDifference.js
@@ -15,3 +15,50 @@ function diagonalDifference(arr) {
 
 
 console.log(diagonalDifference([[1,2,3,4],[4,5,6,8],[9,8,9,9]]))
+
+function countingSort(arr) {
+    const n = arr.length
+    let max = 0
+    
+    for (let i = 0; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i]
+        }
+    }
+    
+    const result = Array(max + 1).fill(0)
+    for (let i = 0; i < n; i++) {
+        result[arr[i]] += 1
+    }
+    return result
+}
+
+
+console.log(countingSort([1,1,3,2,1]))
+
+function countingValleys(steps, path) {
+    let valleyCount = 0
+    let seaLevel = 0
+    let valleyStatus = false
+
+    for (let step of path) {
+        if (step === "U") {
+            seaLevel += 1
+        } else {
+            seaLevel -= 1
+        }
+
+        if (seaLevel < 0 && !valleyStatus) {
+            valleyCount += 1
+            valleyStatus = true
+        } else if (seaLevel >= 0 && valleyStatus) {
+            valleyStatus = false
+        }
+
+    }
+    return valleyCount
+}
+
+console.log(countingValleys(8, "UDDDUDUU"))
+console.log(countingValleys(8, "DDUUUUDD"))
+console.log(countingValleys(12, "DDUUDDUDUUUD"))
