c39f32665dad6c261e6837c4a80fddee581b31d6
Tim Crisp <timothycrisp@gmail.com>
2023-01-10 14:21:17 -0600

    create mod version of zig zag sequence

---
+++ hacker-rank/week-three/zigzagsequence.js
@@ -0,0 +1,10 @@
+function findZigZagSequence(arr) {
+    const n = arr.length
+    const sortedArray = arr.sort((a,b) => a-b)
+    const mid = Math.floor(n/2)
+    const result = [...sortedArray.slice(0, mid), ...sortedArray.slice(mid, n).sort((a,b) => b-a)]
+
+    console.log(result.join(' '))
+}
+
+console.log(findZigZagSequence([2,3,5,1,4]))
