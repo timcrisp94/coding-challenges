2cf35ee677c73e35cf5f900bc7ee901b343e699e
Tim Crisp <timothycrisp@gmail.com>
2023-01-11 10:55:51 -0600

    solve check inclusing using hash and helper function

--- hacker-rank/week-three/permutingTwoArrays.js
+++ hacker-rank/week-three/permutingTwoArrays.js
@@ -34,9 +34,34 @@ function twoArrays(k, A, B) {
 // console.log(twoArrays(10, [2,1,3], [7,8,9]))
 // console.log(twoArrays(5, [1,2,2,1], [3,3,3,4]))
 
-var checkInclusion = function (s1, s2) {
+const checkInclusion = function(s1, s2) {
+    let s = {}, s1sum = 0, s2sum = 0,
+        left = 0, right = s1.length - 1;
     
+    for (let i = 0; i < s1.length; i++) {
+        s1sum += s1.charCodeAt(i);
+        s2sum += s2.charCodeAt(i);
+    }
+    
+    while (right < s2.length) {
+        if (s1sum === s2sum) {
+            if (isTheSameFreq(s1, s2.substring(left, right + 1))) return true
+        }  
+        right++;
+        if (!s2[right]) return false;
+        s2sum = s2sum - s2[left++].charCodeAt(0) + s2[right].charCodeAt(0);
+    }
+    
+    return false;
+    
+    
+    function isTheSameFreq(s1, s2) {
+        let counter = 0;
+        for (let i = 0; i < s1.length; i++)
+            if (s2.includes(s1[i])) counter++;
+        
+        return counter === s1.length;
+    }
 };
-
 console.log(checkInclusion("ab", "eidbaooo"))
 console.log(checkInclusion("ab", "eidboaoo"))
