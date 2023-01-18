c105c25afd1830e63c991d16d3a92eaefc2cfd93
Tim Crisp <timothycrisp@gmail.com>
2023-01-18 15:57:09 -0600

    work on zig zag sequence

--- hacker-rank/week-three/zigzagsequence.js
+++ hacker-rank/week-three/zigzagsequence.js
@@ -1,9 +1,25 @@
 function findZigZagSequence(arr) {
     const n = arr.length
-    const sortedArray = arr.sort((a,b) => a-b)
-    const mid = Math.floor(n/2)
-    const result = [...sortedArray.slice(0, mid), ...sortedArray.slice(mid, n).sort((a,b) => b-a)]
+    const mid = (n-1)/2
+    arr[mid], arr[n-1] = arr[n-1], arr[mid]
 
-    console.log(result.join(' '))
+    let start = mid+1
+    let end = n-1
+
+    while (start <= end) {
+        arr[start], arr[end] = arr[end], arr[start]
+        start = start + 1
+        end = end + 1
+
+        for (let i = 0; i < n; i++) {
+            if (i == n-1) {
+                console.log(arr[i])
+            } else {
+                console.log(arr[i], end = '')
+            }
+        }
+        return
+    }
 }
 
+console.log(findZigZagSequence([7,2,5,4,3,6,1]))
